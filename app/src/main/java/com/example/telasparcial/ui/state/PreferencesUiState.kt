f4a9e606827055c59ea161eff7f1d194043475d1
xHTRx <heitorandra@gmail.com>
2025-11-11 15:06:40 -0300

    feat: create PreferencesUIState.kt

---
+++ app/src/main/java/com/example/telasparcial/ui/state/PreferencesUiState.kt
@@ -0,0 +1,16 @@
+package com.example.telasparcial.ui.state
+
+import androidx.compose.ui.graphics.Color
+
+enum class TamanhoDeFonte(tamanho: Float) {
+    Pequeno(tamanho = 18f),
+    Medio(tamanho = 20f),
+    Grande(tamanho = 22f);
+}
+
+data class PreferencesUiState(
+    val corDeTexto: Color,
+    val corDeFundo: Color,
+    val corDeBotao: Color,
+    val tamanhoDeFonte: TamanhoDeFonte = TamanhoDeFonte.Medio
+)
